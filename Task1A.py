49b02936a1ba03f4617f2ed8c8ffc382b70cab76
jc2336 <jc2336@cam.ac.uk>
2022-02-10 13:38:03 +0000

    10.02.22

--- Task1A.py
+++ Task1A.py
@@ -4,7 +4,6 @@
 
 from floodsystem.stationdata import build_station_list
 
-
 def run():
     """Requirements for Task 1A"""
 
@@ -25,3 +24,4 @@ def run():
 if __name__ == "__main__":
     print("*** Task 1A: CUED Part IA Flood Warning System ***")
     run()
+
